5ce0c47da4ce01f7ed72ce6ca6c99eceb135a64c
Franklin-Kimatu <frankimatu@gmail.com>
2019-11-11 14:01:18 +0300

    add files

--- app/src/main/java/com/wsv/right_light_wsv/AddCustomerFragment.java
+++ app/src/main/java/com/wsv/right_light_wsv/AddCustomerFragment.java
@@ -1,14 +1,35 @@
 package com.wsv.right_light_wsv;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
 public class AddCustomerFragment extends DialogFragment implements View.OnClickListener {
 
 
+    private Button mAddCustomerBtn;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View  rootView =inflater.inflate(R.layout.addcustomerfragment,container,false);
+        getDialog().setTitle("Add customer dialog");
+
+        mAddCustomerBtn =(Button) rootView.findViewById(R.id.addCustomerButton);
+        mAddCustomerBtn.setOnClickListener(this);
+        return rootView;
+    }
+
     @Override
     public void onClick(View v) {
-
+        if (v ==mAddCustomerBtn) {
+            dismiss();
+        }
     }
 }
--- app/src/main/java/com/wsv/right_light_wsv/IndividualCustomerDetails.java
+++ app/src/main/java/com/wsv/right_light_wsv/IndividualCustomerDetails.java
@@ -3,6 +3,8 @@ package com.wsv.right_light_wsv;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class IndividualCustomerDetails extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class IndividualCustomerDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_individual_customer_details);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.editcustomerdetails,menu);
+        return true;
     }
 }
