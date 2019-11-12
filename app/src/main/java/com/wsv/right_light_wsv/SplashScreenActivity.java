6c8ebbee06415c35879649d2a0c8bcc85cde12e0
ferdinandmwal123 <ferdinandmwal@gmail.com>
2019-11-12 15:06:54 +0300

    Changed launch activity to splachscree & set splashscreen length 5s

--- app/src/main/java/com/wsv/right_light_wsv/SplashScreenActivity.java
+++ app/src/main/java/com/wsv/right_light_wsv/SplashScreenActivity.java
@@ -2,7 +2,9 @@ package com.wsv.right_light_wsv;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -10,5 +12,22 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        Thread background = new Thread(){
+            public void run(){
+                try {
+                    sleep(5 * 1000);
+
+                    Intent i = new Intent(getBaseContext(),HomeActivity.class);
+                    startActivity(i);
+
+                    finish();
+
+                } catch (Exception e){
+                    Toast.makeText(SplashScreenActivity.this,"Uknown error",Toast.LENGTH_LONG).show();
+
+                }
+            }
+        };
     }
 }
