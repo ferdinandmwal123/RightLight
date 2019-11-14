010043b1c29306cb2b453a418e7b2e2c234087d3
Franklin-Kimatu <frankimatu@gmail.com>
2019-11-14 14:51:03 +0300

    add an error textview that would display of failure of response.

--- app/src/main/java/com/wsv/right_light_wsv/CustomersActivity.java
+++ app/src/main/java/com/wsv/right_light_wsv/CustomersActivity.java
@@ -15,18 +15,30 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class CustomersActivity extends AppCompatActivity implements View.OnClickListener{
+    //    String [] customerNameList ={"John Doe","Louis Otieno","Robin Mwaura","Ferdinard Thiog'o","Julia Mwong'ina","Kimatu Franklin"};
+
 
     private FloatingActionButton mAddCustomerFloatingBtn;
     RecyclerView mRecyclerView;
-//    String [] customerNameList ={"John Doe","Louis Otieno","Robin Mwaura","Ferdinard Thiog'o","Julia Mwong'ina","Kimatu Franklin"};
+
+
     public List<Customer> mCustomers;
+    TextView errorTextView;
     CustomerListAdapter adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,18 +49,46 @@ public class CustomersActivity extends AppCompatActivity implements View.OnClick
         mAddCustomerFloatingBtn = findViewById(R.id.addCustomerFloatingBtn);
         mAddCustomerFloatingBtn.setOnClickListener(this);
 
+        errorTextView = findViewById(R.id.errorTextView);
+
+        Retrofit retrofit = new Retrofit.Builder().baseUrl("https://rightlight.herokuapp.com/api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        CustomerNamesResponse customerNamesResponse = retrofit.create(CustomerNamesResponse.class);
+
+        Call<List<Customer>> call = customerNamesResponse.getCustomerNames();
+
+        call.enqueue(new Callback<List<Customer>>() {
+            @Override
+            public void onResponse(Call<List<Customer>> call, Response<List<Customer>> response) {
+
+              if (response.isSuccessful()){
+                  mCustomers= response.body();
+
+                  mRecyclerView =(RecyclerView) findViewById(R.id.customerRecyclerView);
+                  mRecyclerView.setLayoutManager(new LinearLayoutManager(CustomersActivity.this));
+                  mRecyclerView.setHasFixedSize(true);
+
+                  adapter = new CustomerListAdapter(mCustomers,CustomersActivity.this);
+                  mRecyclerView.setAdapter(adapter);
+
+              }
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Customer>> call, Throwable t) {
+                errorTextView.setText(t.getMessage());
+            }
+        });
 
 
-        mRecyclerView =(RecyclerView) findViewById(R.id.customerRecyclerView);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-        mRecyclerView.setHasFixedSize(true);
 
 
-        adapter = new CustomerListAdapter(mCustomers,CustomersActivity.this);
-        mRecyclerView.setAdapter(adapter);
 
 
-//        mRecyclerView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+      //        mRecyclerView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 //            @Override
 //            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 //                String itemClickedPosition = (String)adapter.getItem(position);
