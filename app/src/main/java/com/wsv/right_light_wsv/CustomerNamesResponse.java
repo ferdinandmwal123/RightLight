ed127d1f46efa9fe9602475a797249e8abb21616
Franklin-Kimatu <frankimatu@gmail.com>
2019-11-17 12:07:18 +0300

    change the type of customer response to fit the customer model

--- app/src/main/java/com/wsv/right_light_wsv/CustomerNamesResponse.java
+++ app/src/main/java/com/wsv/right_light_wsv/CustomerNamesResponse.java
@@ -3,8 +3,11 @@ package com.wsv.right_light_wsv;
 
 import java.util.List;
 
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 
@@ -13,6 +16,12 @@ public interface CustomerNamesResponse {
     @GET("customer")
     Call<List<Customer>> getCustomerNames();
 
-    @POST("customer")
-    Call<Customer> addCustomer(@Body Customer customer);
+    @FormUrlEncoded
+    @POST("customer/")
+    Call<List<Customer>> addNewCustomer(
+            @Field("Name") String Name,
+            @Field("phone_number") String phone_number,
+            @Field("seller") int seller,
+            @Field("customer_id") String customer_id
+    );
 }
