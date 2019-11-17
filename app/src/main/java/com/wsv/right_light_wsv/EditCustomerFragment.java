314e560587fba4123ab94b669db899bbb675a3a9
Franklin-Kimatu <frankimatu@gmail.com>
2019-11-17 21:16:27 +0300

    add an editCustomer fragment

---
+++ app/src/main/java/com/wsv/right_light_wsv/EditCustomerFragment.java
@@ -0,0 +1,32 @@
+package com.wsv.right_light_wsv;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class EditCustomerFragment extends DialogFragment {
+
+
+    public EditCustomerFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_edit_customer, container, false);
+    }
+
+}
