ef0cf9950a7524f00780e15f1cedf5fe71357399
Franklin-Kimatu <frankimatu@gmail.com>
2019-11-17 12:51:04 +0300

    remove comment out parts

--- app/src/main/java/com/wsv/right_light_wsv/CustomersActivity.java
+++ app/src/main/java/com/wsv/right_light_wsv/CustomersActivity.java
@@ -6,6 +6,7 @@ import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -39,6 +40,7 @@ public class CustomersActivity extends AppCompatActivity implements View.OnClick
     RecyclerView mRecyclerView;
     TextView errorTextView,testCustomerName;
     ProgressBar mProgressBar;
+    SwipeRefreshLayout mRefreshLayout;
 
     public List<Customer> mCustomers;
     CustomerListAdapter adapter;
@@ -57,14 +59,29 @@ public class CustomersActivity extends AppCompatActivity implements View.OnClick
         errorTextView = findViewById(R.id.errorTextView);
         mProgressBar =findViewById(R.id.progress_bar);
         testCustomerName =findViewById(R.id.testCustomerName);
+        mRefreshLayout = findViewById(R.id.refreshLayout);
+
+        mRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                fetchCustomer();
+            }
+        });
+
+
+        fetchCustomer();
+    }
+
+    public void fetchCustomer(){
+
 
         Retrofit retrofit = new Retrofit.Builder().baseUrl("https://rightlight.herokuapp.com/api/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
-         customerNamesResponse = retrofit.create(CustomerNamesResponse.class);
+        customerNamesResponse = retrofit.create(CustomerNamesResponse.class);
 
-       // addCustomer();//posting method
+
 
         Call<List<Customer>> call = customerNamesResponse.getCustomerNames();
 
@@ -75,32 +92,34 @@ public class CustomersActivity extends AppCompatActivity implements View.OnClick
             public void onResponse(Call<List<Customer>> call, Response<List<Customer>> response) {
 
 
-              if (response.isSuccessful()){
-                  mCustomers= response.body();
-                  Collections.sort(mCustomers,Customer.SORT_BY_NAME);
+                if (response.isSuccessful()){
+                    mCustomers= response.body();
+                    Collections.sort(mCustomers,Customer.SORT_BY_NAME);
 
 
-                  mRecyclerView = findViewById(R.id.customerRecyclerView);
-                  mRecyclerView.setLayoutManager(new LinearLayoutManager(CustomersActivity.this));
-                  mRecyclerView.setHasFixedSize(true);
+                    mRecyclerView = findViewById(R.id.customerRecyclerView);
+                    mRecyclerView.setLayoutManager(new LinearLayoutManager(CustomersActivity.this));
+                    mRecyclerView.setHasFixedSize(true);
 
-                  DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(CustomersActivity.this,DividerItemDecoration.VERTICAL);
-                  dividerItemDecoration.setDrawable(getResources().getDrawable(R.drawable.recycler_line_divider));
-                  mRecyclerView.addItemDecoration(dividerItemDecoration);
+                    DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(CustomersActivity.this,DividerItemDecoration.VERTICAL);
+                    dividerItemDecoration.setDrawable(getResources().getDrawable(R.drawable.recycler_line_divider));
+                    mRecyclerView.addItemDecoration(dividerItemDecoration);
 
-                  runOnUiThread(new Runnable() {
-                      public void run() {
-                          adapter = new CustomerListAdapter(mCustomers,CustomersActivity.this);
-                          adapter.notifyDataSetChanged();
-                          mRecyclerView.setAdapter(adapter);
-                      }
-                  });
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            adapter = new CustomerListAdapter(mCustomers,CustomersActivity.this);
+                            adapter.notifyDataSetChanged();
+                            mRecyclerView.setAdapter(adapter);
+                        }
+                    });
 
-                  hideProgressBar();
-              }else{
-                  hideProgressBar();
-                  showFailureMessage();
-              }
+                    mRefreshLayout.setRefreshing(false);
+
+                    hideProgressBar();
+                }else{
+                    hideProgressBar();
+                    showFailureMessage();
+                }
 
 
             }
@@ -111,54 +130,9 @@ public class CustomersActivity extends AppCompatActivity implements View.OnClick
             }
         });
 
-
     }
 
-//    private void addCustomer() {
-//        Customer customer = new Customer("Name","12345",1,"3456789");
-//
-//        Call<List<Customer>> call = customerNamesResponse.addCustomer(customer);
-//
-//
-//        call.enqueue(new Callback<List<Customer>>() {
-//            @Override
-//            public void onResponse(Call<List<Customer>> call, Response<List<Customer>> response) {
-//
-//                if (response.isSuccessful()){
-//                    final List<Customer> addDummyCustomer = response.body();
-//
-//                    Log.i("Adding a customer",addDummyCustomer.toString());
-//                    System.out.println("Adding a customer :" +addDummyCustomer);
-//
-//                    mRecyclerView = findViewById(R.id.customerRecyclerView);
-//                    mRecyclerView.setLayoutManager(new LinearLayoutManager(CustomersActivity.this));
-//                    mRecyclerView.setHasFixedSize(true);
-//
-//                    DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(CustomersActivity.this,DividerItemDecoration.VERTICAL);
-//                    dividerItemDecoration.setDrawable(getResources().getDrawable(R.drawable.recycler_line_divider));
-//                    mRecyclerView.addItemDecoration(dividerItemDecoration);
-//
-//                    runOnUiThread(new Runnable() {
-//                        public void run() {
-//                            adapter = new CustomerListAdapter(addDummyCustomer,CustomersActivity.this);
-//                            adapter.notifyDataSetChanged();
-//                            mRecyclerView.setAdapter(adapter);
-//                        }
-//                    });
-//
-//                }else{
-//                    hideProgressBar();
-//                    showFailureMessage();
-//                }
-//
-//            }
-//
-//            @Override
-//            public void onFailure(Call<List<Customer>> call, Throwable t) {
-//                errorTextView.setText(t.getMessage());
-//            }
-//        });
-//    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
