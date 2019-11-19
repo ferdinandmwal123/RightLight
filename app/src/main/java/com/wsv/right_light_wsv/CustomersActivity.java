c3516141f46551ef20b2cce0acf036d0021d8f29
Louiskoyio <koyiolouis@gmail.com>
2019-11-19 09:47:06 +0300

    aladhjnkd

--- app/src/main/java/com/wsv/right_light_wsv/CustomersActivity.java
+++ app/src/main/java/com/wsv/right_light_wsv/CustomersActivity.java
@@ -1,6 +1,4 @@
 package com.wsv.right_light_wsv;
-
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.FragmentManager;
@@ -8,136 +6,73 @@ import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
-
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.SearchView;
-import androidx.fragment.app.FragmentManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
 import java.util.Collections;
 import java.util.List;
-
 import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
-
 public class CustomersActivity extends AppCompatActivity implements View.OnClickListener{
-
-
     private FloatingActionButton mAddCustomerFloatingBtn;
     RecyclerView mRecyclerView;
-    TextView errorTextView, testCustomerName;
+    TextView errorTextView,testCustomerName;
     ProgressBar mProgressBar;
     SwipeRefreshLayout mRefreshLayout;
-
     public List<Customer> mCustomers;
     CustomerListAdapter adapter;
     CustomerNamesResponse customerNamesResponse;
     private final String BASE1_URL ="https://rightlight.herokuapp.com/api/";
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customers);
-
-
         mAddCustomerFloatingBtn = findViewById(R.id.addCustomerFloatingBtn);
         mAddCustomerFloatingBtn.setOnClickListener(this);
-
         errorTextView = findViewById(R.id.errorTextView);
-        mProgressBar = findViewById(R.id.progress_bar);
-        testCustomerName = findViewById(R.id.testCustomerName);
-
-       
+        mProgressBar =findViewById(R.id.progress_bar);
+        testCustomerName =findViewById(R.id.testCustomerName);
         mRefreshLayout = findViewById(R.id.refreshLayout);
-
-
         mRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
                 fetchCustomer();
             }
         });
-
-
         fetchCustomer();
     }
-
     public void fetchCustomer(){
-
-
         Retrofit retrofit = new Retrofit.Builder().baseUrl(BASE1_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
-
         customerNamesResponse = retrofit.create(CustomerNamesResponse.class);
-
-        // addCustomer();//posting method
-
-
         Call<List<Customer>> call = customerNamesResponse.getCustomerNames();
-
-        Log.i("Here", call.toString());
-
-
+        Log.i("Here",call.toString());
         call.enqueue(new Callback<List<Customer>>() {
             @Override
             public void onResponse(Call<List<Customer>> call, Response<List<Customer>> response) {
-
-
-
-
-                if (response.isSuccessful()) {
-                    mCustomers = response.body();
-                    Log.i("Here", response.body().toString());
-                    Log.i("Ciustomer", mCustomers.toString());
-
-              if (response.isSuccessful()){
-                  mCustomers= response.body();
-                  Log.i("Here",response.body().toString());
-                  Log.i("Ciustomer", mCustomers.toString());
-
-
-                  mRecyclerView = findViewById(R.id.customerRecyclerView);
-                  mRecyclerView.setLayoutManager(new LinearLayoutManager(CustomersActivity.this));
-                  mRecyclerView.setHasFixedSize(true);
-
                 if (response.isSuccessful()){
                     mCustomers= response.body();
                     Collections.sort(mCustomers,Customer.SORT_BY_NAME);
-
-
                     mRecyclerView = findViewById(R.id.customerRecyclerView);
                     mRecyclerView.setLayoutManager(new LinearLayoutManager(CustomersActivity.this));
                     mRecyclerView.setHasFixedSize(true);
-
                     DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(CustomersActivity.this,DividerItemDecoration.VERTICAL);
                     dividerItemDecoration.setDrawable(getResources().getDrawable(R.drawable.recycler_line_divider));
                     mRecyclerView.addItemDecoration(dividerItemDecoration);
-
-                    mRecyclerView = findViewById(R.id.customerRecyclerView);
-                    mRecyclerView.setLayoutManager(new LinearLayoutManager(CustomersActivity.this));
-                    mRecyclerView.setHasFixedSize(true);
-
-                    runOnUiThread(new Runnable() {
-                        public void run() {
-                            adapter = new CustomerListAdapter(mCustomers, CustomersActivity.this);
-                            adapter.notifyDataSetChanged();
-                            mRecyclerView.setAdapter(adapter);
-                        }
-                    });
                     runOnUiThread(new Runnable() {
                         public void run() {
                             adapter = new CustomerListAdapter(mCustomers,CustomersActivity.this);
@@ -145,51 +80,22 @@ public class CustomersActivity extends AppCompatActivity implements View.OnClick
                             mRecyclerView.setAdapter(adapter);
                         }
                     });
-
                     mRefreshLayout.setRefreshing(false);
-
                     hideProgressBar();
                 }else{
                     hideProgressBar();
                     showFailureMessage();
                 }
-
-                    hideProgressBar();
-                } else {
-                    hideProgressBar();
-                    showFailureMessage();
-                }
-//                if (!response.isSuccessful()){
-//                    testCustomerName.setText("Code :" + response.code());
-//                    return;
-//                }
-//                List<Customer> customers =response.body();
-//
-//                for (Customer  customer : customers) {
-//                     String resultCustomerName ="";
-//                     resultCustomerName += "Name "+ customer.getCustomerNames() + "\n";
-//
-//                     testCustomerName.setText(resultCustomerName);
-//                }
-
-
             }
-
             @Override
             public void onFailure(Call<List<Customer>> call, Throwable t) {
                 errorTextView.setText(t.getMessage());
             }
         });
-
     }
-
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-
         getMenuInflater().inflate(R.menu.customersearchmenu,menu);
-
         final MenuItem searchItem = menu.findItem(R.id.customerSearch);
         final SearchView  searchView = (SearchView) searchItem.getActionView();
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
@@ -198,7 +104,6 @@ public class CustomersActivity extends AppCompatActivity implements View.OnClick
                 adapter.getFilter().filter(query);
                 return false;
             }
-
             @Override
             public boolean onQueryTextChange(String newText) {
                 adapter.getFilter().filter(newText);
@@ -207,9 +112,6 @@ public class CustomersActivity extends AppCompatActivity implements View.OnClick
         });
         return true;
     }
-
-
-
     @Override
     public void onClick(View v) {
         if (v== mAddCustomerFloatingBtn){
@@ -217,14 +119,11 @@ public class CustomersActivity extends AppCompatActivity implements View.OnClick
             AddCustomerFragment  addCustomerFragment = new AddCustomerFragment();
             addCustomerFragment.show(fragmentManager,"Add a customer fragment");
         }
-
     }
-
-    public void hideProgressBar() {
+    public void hideProgressBar(){
         mProgressBar.setVisibility(View.GONE);
     }
-
-    public void showFailureMessage() {
+    public void showFailureMessage(){
         errorTextView.setText("Something wen't wrong.Please check your internet connection");
         errorTextView.setVisibility(View.VISIBLE);
     }
