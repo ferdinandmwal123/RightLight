85c22ef64bc5cf645d9e5fbd6621ce6e36291d43
Franklin-Kimatu <frankimatu@gmail.com>
2019-11-19 12:03:51 +0300

    add method to fetch the last date record

--- app/src/main/java/com/wsv/right_light_wsv/IndividualCustomerDetailFragment.java
+++ app/src/main/java/com/wsv/right_light_wsv/IndividualCustomerDetailFragment.java
@@ -10,14 +10,24 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import org.parceler.Parcels;
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 /**
  * A simple {@link Fragment} subclass.
  */
 public class IndividualCustomerDetailFragment extends Fragment {
     private TextView mCustomerIdNoNumber;
     private TextView mCustomerPhoneNoNumber;
+    private TextView mCustomerLastRentalDate;
     private Customer myCustomer;
     private List<Customer> mCustomer;
+    private List<ApiRentResponse>  customerRentRecordHistory;
+    private  ApiService apiService;
+
+
     public IndividualCustomerDetailFragment() {
         // Required empty public constructor
     }
@@ -33,6 +43,26 @@ public class IndividualCustomerDetailFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Customer customer = new Customer();
+
+        apiService = RetrofitClient.getClient().create(ApiService.class);
+        Call<List<ApiRentResponse>> call = apiService.getCustomerRentRecord(3);
+
+        call.enqueue(new Callback<List<ApiRentResponse>>() {
+            @Override
+            public void onResponse(Call<List<ApiRentResponse>> call, Response<List<ApiRentResponse>> response) {
+
+                getLastRentRecord(response.body());
+
+
+            }
+
+            @Override
+            public void onFailure(Call<List<ApiRentResponse>> call, Throwable t) {
+
+            }
+        });
     }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -41,8 +71,18 @@ public class IndividualCustomerDetailFragment extends Fragment {
         View itemView = inflater.inflate(R.layout.fragment_individual_customer_detail, container, false);
         mCustomerIdNoNumber =itemView.findViewById(R.id.customersIdNoNumber);
         mCustomerPhoneNoNumber = itemView.findViewById(R.id.customersPhoneNoNumber);
+        mCustomerLastRentalDate =itemView.findViewById(R.id.customersDateDate);
+
+
+
         mCustomerPhoneNoNumber.setText(myCustomer.getPhoneNumber());
         mCustomerIdNoNumber.setText(myCustomer.getCustomerId());
         return itemView;
+    }
+
+    public void getLastRentRecord(List<ApiRentResponse> customerRentRecordHistory){
+        int lastRecord =customerRentRecordHistory.size() -1;
+        String lastDate = customerRentRecordHistory.get(lastRecord).getRentDate();
+        mCustomerLastRentalDate.setText(lastDate);
     }
 }
