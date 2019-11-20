f07c06f552ae28cf7f577adf1c5a76ea76849c47
Franklin-Kimatu <frankimatu@gmail.com>
2019-11-20 22:07:11 +0300

    add name textview

--- app/src/main/java/com/wsv/right_light_wsv/IndividualCustomerDetailFragment.java
+++ app/src/main/java/com/wsv/right_light_wsv/IndividualCustomerDetailFragment.java
@@ -8,6 +8,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import org.parceler.Parcels;
 import java.util.List;
 
@@ -19,7 +21,7 @@ import retrofit2.Response;
  * A simple {@link Fragment} subclass.
  */
 public class IndividualCustomerDetailFragment extends Fragment {
-    private TextView mCustomerIdNoNumber,mCustomerPhoneNoNumber,mCustomerLateReturnNumber,mCustomerRentalsNumber;
+    private TextView mCustomerIdNoNumber,mCustomerPhoneNoNumber,mCustomerLateReturnNumber,mCustomerRentalsNumber,mCustomerNameDetails;
 
     private TextView mCustomerLastRentalDate;
     private Customer myCustomer;
@@ -33,19 +35,57 @@ public class IndividualCustomerDetailFragment extends Fragment {
     }
     public static IndividualCustomerDetailFragment newInstance(Customer customer){
         IndividualCustomerDetailFragment individualCustomerDetailFragment = new IndividualCustomerDetailFragment();
-//        Bundle args = new Bundle();
+
         individualCustomerDetailFragment.myCustomer = customer;
-//        args.putParcelable("customer", Parcels.wrap(customer));
-//
-//        individualCustomerDetailFragment.setArguments(args);
+
         return individualCustomerDetailFragment;
     }
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        System.out.println("my customer is :" + myCustomer);
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View itemView = inflater.inflate(R.layout.fragment_individual_customer_detail, container, false);
+        mCustomerIdNoNumber =itemView.findViewById(R.id.customersIdNoNumber);
+        mCustomerPhoneNoNumber = itemView.findViewById(R.id.customersPhoneNoNumber);
+        mCustomerLastRentalDate =itemView.findViewById(R.id.customersDateDate);
+        mCustomerLateReturnNumber = itemView.findViewById(R.id.customersLateReturnNumber);
+        mCustomerRentalsNumber = itemView.findViewById(R.id.customersRentalsNumber);
+        mCustomerNameDetails = itemView.findViewById(R.id.customerNameDetails);
 
+
+
+        mCustomerPhoneNoNumber.setText(myCustomer.getPhoneNumber());
+        mCustomerIdNoNumber.setText(myCustomer.getCustomerId());
+        mCustomerNameDetails.setText(myCustomer.getName());
+        return itemView;
+    }
+
+    public void getLastRentRecord(List<ApiRentResponse> customerRentRecordHistory){
+        int lastRecord =customerRentRecordHistory.size() -1;
+        String lastDate = customerRentRecordHistory.get(lastRecord).getRentDate();
+        mCustomerLastRentalDate.setText(lastDate);
+    }
+
+    public void getCountOfLateReturns(List<ApiRentResponse> customerCountOfLateReturn){
+
+
+
+        mCustomerLateReturnNumber.setText(String.valueOf(customerCountOfLateReturn.size()));
+    }
+
+    public void getCustomerRentals(List<ApiRentResponse> customerRentals){
+
+
+        mCustomerRentalsNumber.setText(String.valueOf(customerRentals.size()));
+
+    }
+
+    private void makeApiCall() {
         apiService = RetrofitClient.getClient().create(ApiService.class);
         Call<List<ApiRentResponse>> call = apiService.getCustomerRentRecord(myCustomer.getId());
 
@@ -95,41 +135,11 @@ public class IndividualCustomerDetailFragment extends Fragment {
             }
         });
     }
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View itemView = inflater.inflate(R.layout.fragment_individual_customer_detail, container, false);
-        mCustomerIdNoNumber =itemView.findViewById(R.id.customersIdNoNumber);
-        mCustomerPhoneNoNumber = itemView.findViewById(R.id.customersPhoneNoNumber);
-        mCustomerLastRentalDate =itemView.findViewById(R.id.customersDateDate);
-        mCustomerLateReturnNumber = itemView.findViewById(R.id.customersLateReturnNumber);
-        mCustomerRentalsNumber = itemView.findViewById(R.id.customersRentalsNumber);
-
-
-
-        mCustomerPhoneNoNumber.setText(myCustomer.getPhoneNumber());
-        mCustomerIdNoNumber.setText(myCustomer.getCustomerId());
-        return itemView;
-    }
-
-    public void getLastRentRecord(List<ApiRentResponse> customerRentRecordHistory){
-        int lastRecord =customerRentRecordHistory.size() -1;
-        String lastDate = customerRentRecordHistory.get(lastRecord).getRentDate();
-        mCustomerLastRentalDate.setText(lastDate);
-    }
-
-    public void getCountOfLateReturns(List<ApiRentResponse> customerCountOfLateReturn){
-
-
-
-        mCustomerLateReturnNumber.setText(String.valueOf(customerCountOfLateReturn.size()));
-    }
-
-    public void getCustomerRentals(List<ApiRentResponse> customerRentals){
-
-
-        mCustomerRentalsNumber.setText(String.valueOf(customerRentals.size()));
-
+    public void onResume() {
+        super.onResume();
+        makeApiCall();
+        Toast.makeText(getActivity(), "Fetching details", Toast.LENGTH_SHORT).show();
     }
 }
