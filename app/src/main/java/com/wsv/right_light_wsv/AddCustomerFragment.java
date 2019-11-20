86d0d3bc3e8e584348615a05a71d81a33667bf60
Franklin-Kimatu <frankimatu@gmail.com>
2019-11-20 22:05:22 +0300

    removing comments

--- app/src/main/java/com/wsv/right_light_wsv/AddCustomerFragment.java
+++ app/src/main/java/com/wsv/right_light_wsv/AddCustomerFragment.java
@@ -88,7 +88,7 @@ public class AddCustomerFragment extends DialogFragment implements View.OnClickL
             public void onResponse(Call<List<Customer>> call, Response<List<Customer>> response) {
 
 
-                    List<Customer> addThisCustomer =  response.body();
+
 
                     if (response.isSuccessful()){
                         Toast.makeText(getContext(), "Customer added", Toast.LENGTH_SHORT).show();
